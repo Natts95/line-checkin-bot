c3cea51dc574c76c3aa19e7bd9db3d3537faa4f6
Natts95 <natpitcha@hotmail.com>
2026-01-11 19:01:44 +0700

    first deploy

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('LINE Backend is running 🚀');
+});
+
+app.post('/webhook', (req, res) => {
+  console.log('Webhook received:');
+  console.log(JSON.stringify(req.body, null, 2));
+  res.sendStatus(200);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
